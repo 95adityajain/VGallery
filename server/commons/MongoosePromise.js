51132018471812e6e68f60678955fd7ec0f73246
Aditya Jain <adityajain.783@gmail.com>
2017-01-26 00:23:12 +0530

    Binds exported model object to mongoose object

--- server/commons/MongoosePromise.js
+++ server/commons/MongoosePromise.js
@@ -3,6 +3,8 @@ import Promise from "bluebird";
 
 mongoose.Promise = Promise;
 
+const Schema = mongoose.Schema;
+const model = mongoose.model.bind (mongoose);
 
 export default mongoose;
-//export { Schema, model } = mongoose;
+export { Schema, model };
