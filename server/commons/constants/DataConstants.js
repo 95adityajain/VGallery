4f9e62c097fd334469e760eaf823c0c55d1ddd55
Aditya Jain <adityajain.783@gmail.com>
2017-02-01 17:16:22 +0530

    Added database constants

---
+++ server/commons/constants/DataConstants.js
@@ -0,0 +1,35 @@
+export const CONST = {
+    REDIS_BASE: "vgallery",
+};
+
+export const USERCONST = {
+    BASE: "users",
+
+    FIELD_ID: "_id",
+    FIELD_EMAIL: "email",
+    FIELD_PASSWORD: "password",
+    FIELD_METADATA: "metadata",
+    FIELD_IS_ADULT_CONTENT_ALLOWED: "isAdultContentAllowed",
+    FIELD_IS_REGISTRATION_PENDING: "isRegistrationRequestPending",
+    FIELD_IS_DISABLED: "isDisabled",
+    FIELD_PROFILE: "profile",
+    FIELD_NAME: "name",
+    FIELD_GENDER: "gender",
+    FIELD_PHOTO: "photo",
+    FIELD_PREFERENCES: "preferences",
+    FIELD_GENRES: "genres",
+    FIELD_AUTH: "auth",
+    FIELD_SID: "sid",
+    FIELD_SSID: "ssid",
+
+    VALUE_GENDER_MALE: "male",
+    VALUE_GENDER_FEMALE: "female",
+    VALUE_GENDER_OTHER: "other"
+};
+
+export const GENRECONST = {
+    BASE: "genres",
+
+    FIELD_ID: "_id",
+    FIELD_NAME: "name"
+};
